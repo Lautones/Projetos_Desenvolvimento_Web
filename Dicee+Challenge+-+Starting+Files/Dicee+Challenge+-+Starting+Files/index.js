9cf9e1e3101fb7879674d6deea1f66e86ac20ff8
Matheus Lauton <mlautonsantana@gmail.com>
2024-07-19 15:47:31 -0300

    Add files via upload

---
+++ Dicee+Challenge+-+Starting+Files/Dicee+Challenge+-+Starting+Files/index.js
@@ -0,0 +1,15 @@
+var randomNumber1 = Math.round(Math.random()*5+1);
+var randomNumber2 = Math.round(Math.random()*5+1);
+
+document.querySelector(".img").setAttribute("src","./images/dice"+randomNumber1+".png");
+document.querySelector(".img2").setAttribute("src","./images/dice"+randomNumber2+".png");
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").textContent="Player 1 Wins!";
+}
+else if(randomNumber2>randomNumber1){
+    document.querySelector("h1").textContent="Player 2 Wins!";
+}
+else{
+    document.querySelector("h1").textContent="Draw!";
+}
